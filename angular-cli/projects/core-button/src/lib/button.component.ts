513cfdbb4455c26c1d589f6f18bfbc58c29b4011
Dominique Müller <dominique.m.mueller@gmail.com>
2024-09-14 21:25:02 +0200

    Add Angular CLI monorepo

---
+++ angular-cli/projects/core-button/src/lib/button.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'scope-core-button',
+  standalone: true,
+  templateUrl: './button.component.html',
+  styleUrls: [
+    './button.component.css'
+  ]
+})
+export class CoreButtonComponent {}
